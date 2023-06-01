9d1c24e29802e8414f4170fb93753e24f1d1fc1c
yaswanth narikamalli <yaswanthnarikamali@gmail.com>
2023-06-01 16:36:46 +0530

    functionality completed

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // const API_KEY="99a9d9233c0192aba6435485fa9fa71a";
 const url="https://gnews.io/api/v4/search?q=";
-
-
+const max=20;
 window.addEventListener('load',() => fetchNews("India"));
 
 async function fetchNews(query){
-    const res= await fetch(`${url}${query}&apikey=${API_KEY}`);
+    const res= await fetch(`${url}${query}&max=20&apikey=${API_KEY}`);
     const data= await res.json();
     console.log(data);
     bindData(data.articles);
@@ -19,8 +18,7 @@ function bindData(articles){
 
     articles.forEach((article) => {
 
-        if(!article.image) return;
-        const cardClone=newsCardTemplate.content.cloneNode(true);
+        const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article);
         cardsContainer.appendChild(cardClone);
         
@@ -37,7 +35,40 @@ function fillDataInCard(cardClone,article){
     newsTitle.innerHTML=article.title;
     newsSource.innerHTML=article.source.name;
     newsDesc.innerHTML=article.description;
+
+    // const date=new Date(article.publishedAt).toLocaleDateString("en-US", {
+    //     timeZone:"Asia/Vijayawada",
+    // });
+
+    // newsSource.innerHTML=`${article.source.name} . ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click",()=>{
+        window.open(article.url,"_blank");
+    });
 }
+
+
+let curSelectedNav=null;
+function navItemClick(id){
+    fetchNews(id);
+    const navItem=document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav=navItem;
+    curSelectedNav.classList.add("active");
+
+
+}
+
+const searchButton=document.getElementById('search-button');
+const searchText=document.getElementById('search-text');
+
+searchButton.addEventListener("click",() => {
+    const query=searchText.value;
+    if(!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav=null;
+})
 
 // const cardsContainer=document.getElementById("cards-container");
 // const newsCardTemplate=document.getElementById("template-news-card");
